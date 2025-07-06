bda5e93659445ed43f08046093db474cac3e4b70
Dweep Vira <79634266+dweepvira@users.noreply.github.com>
2025-07-06 19:15:50 +0530

    Update settings.py

--- aedp_project/settings.py
+++ aedp_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dlaxtl-=*%ggv%3qgm!hbv10s$s$bfu*wr^!a3yr4!#&r!zg_5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
