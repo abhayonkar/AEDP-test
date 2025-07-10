e2ef024cfed5be7c6aa71b946cd084a42b604984
Dweep Vira <79634266+dweepvira@users.noreply.github.com>
2025-07-10 11:50:05 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,11 @@
 set -e
 
 # Check if the DJANGO_MANAGE_COMMAND variable is set
-if [ -n "$DJANGO_MANAGE_COMMAND" ]; then
-    # If it is set, run that command
-    echo "Running Django management command: $DJANGO_MANAGE_COMMAND"
-    python3 manage.py $DJANGO_MANAGE_COMMAND
-else
-    # Otherwise, start the web server as normal
-    echo "Applying database migrations..."
-    python3 manage.py migrate
 
-    echo "Starting Gunicorn server..."
-    exec gunicorn aedp_project.wsgi:application --bind 0.0.0.0:10000
-fi
+    # Otherwise, start the web server as normal
+echo "Applying database migrations..."
+python3 manage.py migrate
+
+echo "Starting Gunicorn server..."
+exec gunicorn aedp_project.wsgi:application --bind 0.0.0.0:10000
+
